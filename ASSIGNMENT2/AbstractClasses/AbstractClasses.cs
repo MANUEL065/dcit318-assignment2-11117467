c57934bd37dadaa6051f52f7ee97e1e9a5cfc62d
Emmanuel Mensah <ekmensah065@st.ug.edu.gh>
2025-08-07 01:39:29 +0000

    Task 2: Abstract Classes and Methods

---
+++ ASSIGNMENT2/AbstractClasses/AbstractClasses.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace ASSIGNMENT2.AbstractClasses
+{
+    public class AbstractDemo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Abstract Classes and Methods ===");
+
+            Shape circle = new Circle(5);
+            Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
+
+            Shape rectangle = new Rectangle(4, 6);
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea():F2}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+}
