6aa1aefef473ac9b65f482be509b58fcf99cbeeb
Emmanuel Mensah <ekmensah065@st.ug.edu.gh>
2025-08-07 01:20:47 +0000

    Task 1: Inheritance and Method Overriding

---
+++ ASSIGNMENT2/InheritanceMethodOveride/InheritanceMethodOveride.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace ASSIGNMENT2.InheritanceMethodOveride
+{
+    public class InheritanceMethodOveride
+    {
+        public static void Run()
+        {
+            Console.WriteLine("=== Inheritance and Method Overriding ===");
+
+            Animal generic = new Animal();
+            generic.MakeSound();
+
+            Animal dog = new Dog();
+            dog.MakeSound();
+
+            Animal cat = new Cat();
+            cat.MakeSound();
+        }
+    }
+
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
---
+++ ASSIGNMENT2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ASSIGNMENT2.InheritanceMethodOveride; // Import the namespace of your Animal classes
+
+namespace ASSIGNMENT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Inheritance and Method Overriding ===");
+
+            Animal generic = new Animal();
+            generic.MakeSound(); // Some generic sound
+
+            Animal dog = new Dog();
+            dog.MakeSound(); // Bark
+
+            Animal cat = new Cat();
+            cat.MakeSound(); // Meow
+        }
+    }
+}
